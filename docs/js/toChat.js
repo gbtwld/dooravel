af4567fb7188c2aae2dd82f66bb9676e5631d604
dujaejeong <gbtwld@gmail.com>
2020-09-14 16:13:28 +0900

    Delete Safari smooth scroll

--- docs/js/toChat.js
+++ docs/js/toChat.js
@@ -1,23 +1,13 @@
 const toChatBtn = document.querySelector(".toChat__btn");
-const chat = document.querySelector("#disqus_thread");
 
 const toChatHandler = () => {
-  SmoothVerticalScrolling(toChatBtn, 275, chat);
+  window.location.href = "#disqus_thread";
+  toChatBtn.classList.add("toChat__btn__clicked");
 };
 
-function SmoothVerticalScrolling(e, time, where) {
-  var eTop = e.getBoundingClientRect().top;
-  var eAmt = eTop / 100;
-  var curTime = 0;
-  while (curTime <= time) {
-    window.setTimeout(SVS_B, curTime, eAmt, where);
-    curTime += time / 100;
-  }
-}
-
-function SVS_B(eAmt, where) {
-  if (where == "center" || where == "") window.scrollBy(0, eAmt / 2);
-  if (where == "top") window.scrollBy(0, eAmt);
-}
+const scrollHandler = () => {
+  const currentScrollPosition = window.scrollY;
+};
 
 toChatBtn.addEventListener("click", toChatHandler);
+document.addEventListener("scroll", scrollHandler);
