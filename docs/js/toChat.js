b8c546e973f1b65f3b2e607535134952bbda3f88
dujaejeong <gbtwld@gmail.com>
2020-09-14 16:00:59 +0900

    Correct smooth scroll doesn't work  in safri

--- docs/js/toChat.js
+++ docs/js/toChat.js
@@ -1,14 +1,24 @@
 const toChatBtn = document.querySelector(".toChat__btn");
+const chat = document.querySelector("#disqus_thread");
 
 const toChatHandler = () => {
-  window.location.href = "#disqus_thread";
-  toChatBtn.classList.add("toChat__btn__clicked");
+  SmoothVerticalScrolling(toChatBtn, 275, chat);
 };
 
-const scrollHandler = () => {
-  const currentScrollPosition = window.scrollY;
-  console.log(currentScrollPosition);
-};
+function SmoothVerticalScrolling(e, time, where) {
+  var eTop = e.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+    window.setTimeout(SVS_B, curTime, eAmt, where);
+    curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, where) {
+  if (where == "center" || where == "") window.scrollBy(0, eAmt / 2);
+  if (where == "top") window.scrollBy(0, eAmt);
+}
 
 toChatBtn.addEventListener("click", toChatHandler);
 document.addEventListener("scroll", scrollHandler);
